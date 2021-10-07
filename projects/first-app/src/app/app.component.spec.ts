d7255f290976ec55b1b7fa7d96c579c8b785df43
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2021-10-06 20:28:04 -0500

    [feat] second app

--- projects/first-app/src/app/app.component.spec.ts
+++ projects/first-app/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('first-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('first-app app is running!');
-  });
 });
